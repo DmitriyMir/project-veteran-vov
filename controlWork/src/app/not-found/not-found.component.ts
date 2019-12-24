6c8fc17a441e1ef91fcc028b9ad55bd6948bd888
Tapok <abram337@mail.ru>
2019-12-24 15:52:01 +0300

    Много пользователей/Pipes

--- controlWork/src/app/not-found/not-found.component.ts
+++ controlWork/src/app/not-found/not-found.component.ts
@@ -3,10 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.css']
-})
-export class NotFoundComponent implements OnInit {
+  styleUrls: ['./not-found.component.css'],
 
+})
+
+export class NotFoundComponent implements OnInit {
+  myDate = Date.now();
+  Number1: number = 2019.343476386448;
+  Message1: string = "Angular is awesome!";
+  Message2: string = "angular";
+  Message3: string = "AWESOME";
   constructor() { }
 
   ngOnInit() {
